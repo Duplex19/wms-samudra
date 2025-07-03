996e97c52c2fa7c3049cc10094287fd0ad6309bb
umaedi <umaedi.kh.99@gmail.com>
2025-07-03 17:03:15 +0700

    test auto deploy

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="app-brand demo">
     <a href="#" class="app-brand-link">
         <img src="{{ asset('assets/img/logo/samudra-wasesa.png') }}" alt="logo" width="35">
-        <span class="app-brand-text demo menu-text fw-bolder ms-2" style="color: #2563eb">Samudra</span>
+        <span class="app-brand-text demo menu-text fw-bolder ms-2" style="color: #2563eb">Samudra Wa</span>
     </a>
 
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
