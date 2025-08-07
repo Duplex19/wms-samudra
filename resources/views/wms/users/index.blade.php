2313e273d0f5f9133c8bbdbf8b410669dae3c3a6
ariffzi <arffuzi@gmail.com>
2025-08-07 16:46:10 +0700

    fix

--- resources/views/wms/users/index.blade.php
+++ resources/views/wms/users/index.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 @section('content')
-    <button class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#addVpnServer"><i class="menu-icon icon-base bx bx-user"></i>
-    Tambah user</button>
+    <button class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#addVpnServer"><i
+            class="menu-icon icon-base bx bx-user"></i>
+        Tambah user</button>
     <div class="row mb-12 g-6" id="team">
         <div class="col-md-4">
             <div class="card" aria-hidden="true">
                 <div class="card-body">
                     <h5 class="card-title placeholder-glow">
-                    <span class="placeholder col-6"></span>
+                        <span class="placeholder col-6"></span>
                     </h5>
                     <p class="card-text placeholder-glow">
-                    <span class="placeholder col-7"></span>
-                    <span class="placeholder col-4"></span>
-                    <span class="placeholder col-4"></span>
-                    <span class="placeholder col-6"></span>
-                    <span class="placeholder col-8"></span>
+                        <span class="placeholder col-7"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-6"></span>
+                        <span class="placeholder col-8"></span>
                     </p>
                     <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
                 </div>
@@ -24,14 +25,14 @@
             <div class="card" aria-hidden="true">
                 <div class="card-body">
                     <h5 class="card-title placeholder-glow">
-                    <span class="placeholder col-6"></span>
+                        <span class="placeholder col-6"></span>
                     </h5>
                     <p class="card-text placeholder-glow">
-                    <span class="placeholder col-7"></span>
-                    <span class="placeholder col-4"></span>
-                    <span class="placeholder col-4"></span>
-                    <span class="placeholder col-6"></span>
-                    <span class="placeholder col-8"></span>
+                        <span class="placeholder col-7"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-6"></span>
+                        <span class="placeholder col-8"></span>
                     </p>
                     <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
                 </div>
@@ -41,100 +42,100 @@
             <div class="card" aria-hidden="true">
                 <div class="card-body">
                     <h5 class="card-title placeholder-glow">
-                    <span class="placeholder col-6"></span>
+                        <span class="placeholder col-6"></span>
                     </h5>
                     <p class="card-text placeholder-glow">
-                    <span class="placeholder col-7"></span>
-                    <span class="placeholder col-4"></span>
-                    <span class="placeholder col-4"></span>
-                    <span class="placeholder col-6"></span>
-                    <span class="placeholder col-8"></span>
+                        <span class="placeholder col-7"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-6"></span>
+                        <span class="placeholder col-8"></span>
                     </p>
                     <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="editUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Edit user</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-
-      <form action="" id="updateUser" enctype="multipart/form-data">
-        @method('PUT')
-        @csrf
-      <div class="modal-body">
-        <div class="form-group mb-3">
-            <label for="">Manjemen tim</label>
-            <select class="form-select" name="team_management_id">
-            </select>
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Nama tim</label>
-            <input type="text" class="form-control" name="team">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Nama</label>
-            <input type="text" class="form-control" name="name">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Email</label>
-            <input type="text" class="form-control" name="email">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Nomor WhatsApp</label>
-            <input type="text" class="form-control" name="whatsapp">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Password</label>
-            <input type="text" class="form-control" name="password">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Jabatan</label>
-            <input type="text" class="form-control" name="jabatan">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Foto</label>
-            <input type="file" class="form-control" name="foto">
-        </div>
-        <div class="form-group">
-            <label for="">Role</label>
-            <select class="form-select" name="role">
-                <option value="admin">admin</option>
-                <option value="management">management</option>
-                <option value="teknisi">teknisi</option>
-            </select>
-        </div>
-        <hr>
-        <label for="" class="fw-bold mb-2">Akun bank</label>
-        <div class="form-group mb-3">
-            <label for="">Nama</label>
-            <input type="text" class="form-control" id="namaPemilikRekening">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Nama bank</label>
-            <input type="text" class="form-control" name="bank_code">
-        </div>
-        <div class="form-group mb-3">
-            <label for="">Nomor rekening</label>
-            <input type="text" class="form-control" name="norek">
-        </div>
-      </div>
-      <div class="modal-footer">
-        <x-btnLoading id="btnLoading" />
-        <x-btnSubmit text="Perbaharui" id="btnSubmit" onclick="loading(true, 'btnSubmit','btnLoading')" />
-      </div>
-      </form>
     </div>
-  </div>
-</div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="editUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit user</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <form action="" id="updateUser" enctype="multipart/form-data">
+                    @method('PUT')
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group mb-3">
+                            <label for="">Manajemen tim</label>
+                            <select class="form-select" name="team_management_id">
+                            </select>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Nama tim</label>
+                            <input type="text" class="form-control" name="team">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Nama</label>
+                            <input type="text" class="form-control" name="name">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Email</label>
+                            <input type="text" class="form-control" name="email">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Nomor WhatsApp</label>
+                            <input type="text" class="form-control" name="whatsapp">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Password</label>
+                            <input type="text" class="form-control" name="password">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Jabatan</label>
+                            <input type="text" class="form-control" name="jabatan">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Foto</label>
+                            <input type="file" class="form-control" name="foto">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Role</label>
+                            <select class="form-select" name="role">
+                                <option value="admin">admin</option>
+                                <option value="management">management</option>
+                                <option value="teknisi">teknisi</option>
+                            </select>
+                        </div>
+                        <hr>
+                        <label for="" class="fw-bold mb-2">Akun bank</label>
+                        <div class="form-group mb-3">
+                            <label for="">Nama</label>
+                            <input type="text" class="form-control" id="namaPemilikRekening">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Nama bank</label>
+                            <input type="text" class="form-control" name="bank_code">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Nomor rekening</label>
+                            <input type="text" class="form-control" name="norek">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <x-btnLoading id="btnLoading" />
+                        <x-btnSubmit text="Perbaharui" id="btnSubmit"
+                            onclick="loading(true, 'btnSubmit','btnLoading')" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
 @push('js')
     <script>
@@ -154,9 +155,8 @@
             });
         });
 
-        function editUser(user)
-        {
-            $("#updateUser").attr('action', '/wms/users/update/'+user.id);
+        function editUser(user) {
+            $("#updateUser").attr('action', '/wms/users/update/' + user.id);
             $("input[name=name]").val(user.name)
             $("input[name=email]").val(user.email)
             $("input[name=role]").val(user.role)
@@ -167,9 +167,9 @@
             $("input[name=team]").val(user.team)
             $("input[name=team]").val(user.team)
             //akun bank
-            $("#namaPemilikRekening").val(user.bank.name)
+            $("#namaPemilikRekening").val(user.bank.account_name)
             $("input[name=bank_code]").val(user.bank.bank_code)
-            $("input[name=norek]").val(user.bank.norek)
+            $("input[name=norek]").val(user.bank.account_number)
             $("#editUser").modal('show');
         }
     </script>
